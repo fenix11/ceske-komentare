949a25a6aafe0413143318337f3a71ffa0b0027f
fenix11 <fenix11@users.noreply.github.com>
2014-06-12 18:21:43 +0200

    Update ceske-komentare.php

--- ceske-komentare.php
+++ ceske-komentare.php
@@ -3,7 +3,7 @@
 Plugin Name:       České komentáře
 Plugin URI:        https://github.com/fenix11/ceske-komentare
 Description:       A plugin to automatically update GitHub or Bitbucket hosted plugins and themes into WordPress. Plugin class based upon <a href="https://github.com/codepress/github-plugin-updater">codepress/github-plugin-updater</a>. Theme class based upon <a href="https://github.com/WordPress-Phoenix/whitelabel-framework">Whitelabel Framework</a> modifications.
-Version:           1.9
+Version:           2.0
 Author:            fenixx
 License:           GNU General Public License v2
 License URI:       http://www.gnu.org/licenses/gpl-2.0.html
@@ -22,15 +22,10 @@ if ( is_admin() && ( ! defined( 'DOING_AJAX' ) || ! DOING_AJAX ) ) {
 	if ( ! class_exists( 'GitHub_Updater' ) ) {
 		require_once 'includes/class-github-updater.php';
 		require_once 'includes/class-github-api.php';
-		require_once 'includes/class-bitbucket-api.php';
 	}
 	if ( ! class_exists( 'GitHub_Plugin_Updater' ) ) {
 		require_once 'includes/class-plugin-updater.php';
 		new GitHub_Plugin_Updater;
-	}
-	if ( ! class_exists( 'GitHub_Theme_Updater' ) ) {
-		require_once 'includes/class-theme-updater.php';
-		new GitHub_Theme_Updater;
 	}
 }
 
