5ab87cf1908b5ab3cc5e368a1bda564cb7b8b840
fenix11 <fenix11@users.noreply.github.com>
2014-06-19 17:09:12 +0200

    Update ceske-komentare.php

--- ceske-komentare.php
+++ ceske-komentare.php
@@ -5,6 +5,7 @@ Plugin URI:        https://github.com/fenix11/ceske-komentare
 Description:       A plugin to automatically update GitHub or Bitbucket hosted plugins and themes into WordPress. Plugin class based upon <a href="https://github.com/codepress/github-plugin-updater">codepress/github-plugin-updater</a>. Theme class based upon <a href="https://github.com/WordPress-Phoenix/whitelabel-framework">Whitelabel Framework</a> modifications.
 Version:           2.3
 Author:            fenixx
+Author URI:        http://blog.doprofilu.cz
 License:           GNU General Public License v2
 License URI:       http://www.gnu.org/licenses/gpl-2.0.html
 Domain Path:       /languages
@@ -15,44 +16,6 @@ GitHub Branch:     master
 // If this file is called directly, abort.
 if ( ! defined( 'WPINC' ) ) {
 	die;
-}
-
-// Load base classes and Launch
-if ( is_admin() && ( ! defined( 'DOING_AJAX' ) || ! DOING_AJAX ) ) {
-	if ( ! class_exists( 'GitHub_Updater' ) ) {
-		require_once 'includes/class-github-updater.php';
-		require_once 'includes/class-github-api.php';
-	}
-	if ( ! class_exists( 'GitHub_Plugin_Updater' ) ) {
-		require_once 'includes/class-plugin-updater.php';
-		new GitHub_Plugin_Updater;
-	}
-}
-
-function pridat() {
-
-    // Activation code here...
-	add_option( 'pocet0', 'Žádný komentář', '', 'yes' );
-	add_option( 'pocet1', '1 komentář', '', 'yes' );
-	add_option( 'pocet2', '% komentáře', '', 'yes' );
-	update_option( 'pocet5', '% komentářů', '', 'yes' );
-}
-register_activation_hook( __FILE__, 'pridat' );
-
-$pocet0=get_option('pocet0');
-$pocet1=get_option('pocet1');
-$pocet2=get_option('pocet2');
-$pocet5=get_option('pocet5');
-
-add_action( 'admin_menu', 'register_my_custom_menu_page' );
-
-function register_my_custom_menu_page(){
-    add_menu_page( 'Administrace - české komentáře', 'České komentáře', 'manage_options', 'custompage', 'my_custom_menu_page', 'dashicons-format-status', 59 ); 
-}
-
-function my_custom_menu_page(){
-echo '<h1>Administrace</h1>';
-   include('nastaveni.php');
 }
 
 function ceske_komentare($output, $number ){
