b34fa35a35811a5523fa03e2d49b5f5a999cd159
fenix11 <fenix11@users.noreply.github.com>
2014-06-19 18:10:35 +0200

    Update ceske-komentare.php

--- ceske-komentare.php
+++ ceske-komentare.php
@@ -30,17 +30,45 @@ if ( is_admin() && ( ! defined( 'DOING_AJAX' ) || ! DOING_AJAX ) ) {
 	}
 }
 
-function cz_ceske_komentare($output, $number){ if ( $number == 0) $output = 'Žádný komentář';
+function pridat() {
+    // Activation code here...
+	add_option( 'pocet0', 'Žádný komentář', '', 'yes' );
+	add_option( 'pocet1', '1 komentář', '', 'yes' );
+	add_option( 'pocet2', '% komentáře', '', 'yes' );
+	update_option( 'pocet5', '% komentářů', '', 'yes' );
+}
+register_activation_hook( __FILE__, 'pridat' );
+
+$pocet0=get_option('pocet0');
+$pocet1=get_option('pocet1');
+$pocet2=get_option('pocet2');
+$pocet5=get_option('pocet5');
+
+add_action( 'admin_menu', 'register_my_custom_menu_page' );
+
+function register_my_custom_menu_page(){
+    add_menu_page( 'Administrace - české komentáře', 'České komentáře', 'manage_options', 'nastaveni', 'my_custom_menu_page', 'dashicons-format-status', 59 ); 
+}
+
+function my_custom_menu_page(){
+echo '<h1>Administrace</h1>';
+   include('nastaveni.php');
+}
+
+function ceske_komentare($output, $number ){
+global $pocet0,$pocet1,$pocet2,$pocet5;
+if ( $number == 0) $output = $pocet0;
 elseif ($number == 1 )
-$output = str_replace('%', number_format_i18n($number), '% komentář');
+$output = str_replace('%', number_format_i18n($number), $pocet1);
 elseif ($number > 1  and $number < 4 )
-$output = str_replace('%', number_format_i18n($number), '% komentáře');
+$output = str_replace('%', number_format_i18n($number), $pocet2);
 else
-$output = str_replace('%', number_format_i18n($number), '% komentářů');
+$output = str_replace('%', number_format_i18n($number), $pocet5);
 
 return $output; } 
 
 
-add_action('comments_number', 'cz_ceske_komentare', 10, 2);
+
+add_action('comments_number', 'ceske_komentare', 10, 2);
 
 ?>
