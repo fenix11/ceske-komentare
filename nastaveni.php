b29d4f22e4f8298ae78ed1ffcf212d50f63e03cb
fenix11 <fenix11@users.noreply.github.com>
2014-06-20 08:37:01 +0200

    Update nastaveni.php

--- nastaveni.php
+++ nastaveni.php
@@ -28,7 +28,7 @@ if ( false !== $_POST['updated']) : ?>
 <label class="left" for="pocet0"><b>Text při žádném komentáři</b>: </label><input type="text" id="pocet0" name="pocet0" value="<?php echo get_option('pocet0')?>"></br>
 <label class="left" for="pocet1"><b>Text při 1 komentáři</b>:</label><input id="pocet1" type="text" name="pocet1" value="<?php echo get_option('pocet1')?>"></br>
 <label class="left" for="pocet2"><b>Text při 2 - 4 komentářích</b>:</label><input id="pocet2" type="text" name="pocet2" value="<?php echo get_option('pocet2')?>"></br>
-<label class="left" for="pocet5"><b>Text při více jak 4 komentáříh (5,6,...)</b>:</label><input id="pocet5" type="text" name="pocet5" value="<?php echo get_option('pocet5')?>"><br>
+<label class="left" for="pocet5"><b>Text při více jak 4 komentářích (5,6,...)</b>:</label><input id="pocet5" type="text" name="pocet5" value="<?php echo get_option('pocet5')?>"><br>
 <input type="hidden" name="updated">
 <b>Pozn: Je možné používat zástupný znak "%" pro dosazení aktuálního počtu komentářů</b>
 <?php submit_button ('Uložit nastavení'); ?>
